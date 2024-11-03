4acf4254436839bad643b2a950248c7a22c92ffa
lagharimuhammad <lagharimbaqar@gmail.com>
2024-11-03 19:22:48 +0500

    Update detector.c

--- PF LAB 4/detector.c
+++ PF LAB 4/detector.c
@@ -7,7 +7,7 @@ char ch;
 printf("input a character");
 scanf("%c", &a);
 
-if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
 {
 printf("its a vowel");
 }
@@ -19,14 +19,13 @@ printf("its a digit");
 }
 else
 {
-if(ch >= 'a' || ch <= 'z')
+if(ch >= 'a' || ch <= 'z' || ch >= 'A' || ch <= 'Z')
 {
 printf("its a consonant");
 }
 else
 {
 printf("it is a special charcter ");
-}
 }
 
 retrun 0;
